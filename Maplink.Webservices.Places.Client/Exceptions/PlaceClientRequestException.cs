4f989642516c169aca2d284bb73067c8a8a2c569
Ricardo Tealdi <ricardo.tealdi@gmail.com>
2011-08-03 17:47:19 -0300

    added status code name on exception message

--- Maplink.Webservices.Places.Client/Exceptions/PlaceClientRequestException.cs
+++ Maplink.Webservices.Places.Client/Exceptions/PlaceClientRequestException.cs
@@ -1,14 +1,15 @@
 using System;
+using System.Net;
 
 namespace Maplink.Webservices.Places.Client.Exceptions
 {
     public class PlaceClientRequestException : Exception
     {
         private const string MessageTemplate 
-            = "An error occurred while requesting the webservice. The response status code was {0}";
+            = "An error occurred while requesting the webservice. The response status code was {0} {1}";
         
         public PlaceClientRequestException(int statusCode)
-            : base(String.Format(MessageTemplate, statusCode))
+            : base(String.Format(MessageTemplate, statusCode, (HttpStatusCode) statusCode))
         {
         }
     }
