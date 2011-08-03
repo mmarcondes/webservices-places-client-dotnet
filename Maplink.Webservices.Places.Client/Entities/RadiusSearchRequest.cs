00eee7979748144a7def666aad8f9126bce245ac
Ricardo Tealdi <ricardo.tealdi@gmail.com>
2011-08-03 14:18:06 -0300

    set login and key as empty string on radius search request constructor to avoid any null reference

--- Maplink.Webservices.Places.Client/Entities/RadiusSearchRequest.cs
+++ Maplink.Webservices.Places.Client/Entities/RadiusSearchRequest.cs
@@ -1,7 +1,15 @@
+using System;
+
 namespace Maplink.Webservices.Places.Client.Entities
 {
     public class RadiusSearchRequest
     {
+        public RadiusSearchRequest()
+        {
+            Login = String.Empty;
+            Key = String.Empty;
+        }
+
         public int Radius { get; set; }
         public double Latitude { get; set; }
         public double Longitude { get; set; }
