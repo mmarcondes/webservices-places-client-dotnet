7c991e034181df4a1c58bcae295f05333651c225
Ricardo Tealdi <ricardo.tealdi@gmail.com>
2011-08-10 18:25:50 -0300

    create PlaceSearchPaginationRequest class

---
+++ Maplink.Webservices.Places.Client/Entities/PlaceSearchPaginationRequest.cs
@@ -0,0 +1,9 @@
+namespace Maplink.Webservices.Places.Client.Entities
+{
+    public class PlaceSearchPaginationRequest
+    {
+        public string Uri { get; set; }
+        public string Login { get; set; }
+        public string Key { get; set; }
+    }
+}
